5ec8bc429cfc16029d53d0f43b1c16cbb4fca61f
Alimobasheri <a.mobasheri@icloud.com>
2020-11-25 16:14:55 +0330

    Create middleware to verify user token

---
+++ server/middleware/require-login.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+
+module.exports = (req, res, next) => {
+    const {authorization} = req.headers
+    if (!authorization) {
+        return res.status(422).json({error: "you must be logged in."})
+    }
+    
+    const token = authorization.replace("Bearer ", "")
+    jwt.verify(token, JWT_SECRET, (error, payload) => {
+        if (error) {
+            return res.status(422).json({error: "you must be logged in."})
+        }
+
+        const {_id} = payload
+        User.findById(_id)
+            .then(foundUser => {
+                req.user = foundUser
+            })
+        next()
+    })
+}
