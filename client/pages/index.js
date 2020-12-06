16ae84c97b0ec04b151a85fba23fe45ddd7a558b
Alimobasheri <a.mobasheri@icloud.com>
2020-12-06 22:59:16 +0330

    create home page feed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,149 @@
 import Head from 'next/head'
+
+import { Avatar, Box, Card, CardContent, CardHeader, CardMedia, Grid, makeStyles, Typography, fade, Button, IconButton } from '@material-ui/core'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined'
+import ShareIcon from '@material-ui/icons/Share'
 import styles from '../styles/Home.module.css'
 
+const feedPosts = [
+  {
+    media: "p2.jpg",
+    postedBy: {
+      name: "جعفر هاشمی",
+      user_name: "jaffar_hashemi"
+    },
+    location: "تهرانپارس، تهران",
+    text: "یه روز خوب! #kos",
+    likesCount: 45
+  },
+  {
+    media: "p3.jpg",
+    postedBy: {
+      name: "جعفر هاشمی",
+      user_name: "jaffar_hashemi"
+    },
+    location: "سهروردی، تهران",
+    text: "دریاب که از روح جدا خواهی رفت / در پرده ی اسرار فنا خواهی رفت. می نوش ندانی ز کجا آمده ای / خوش باش ندانی به کجا خواهی",
+    likesCount: 820
+  },
+  {
+    media: "p4.jpg",
+    postedBy: {
+      name: "جعفر هاشمی",
+      user_name: "jaffar_hashemi"
+    },
+    location: "لاهیجان، گیلان",
+    text: "نیومدی و رفتیم...",
+    likesCount: 360
+  }
+]
+
+const useHomeStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "stretch"
+  },
+  postsWrapper: {
+    padding: `${theme.spacing(1)}px ${theme.spacing(5)}px`,
+    [theme.breakpoints.down("sm")]: {
+      padding: theme.spacing(1)
+    }
+  },
+  postCard: {
+    margin: theme.spacing(3),
+    backgroundColor: fade(theme.palette.primary.contrastText, 0.07),
+    borderRadius: theme.spacing(2)
+  },
+  postPhoto: {
+    width: '100%',
+    height: theme.spacing(40),
+    boxShadow: theme.shadows[8],
+    borderRadius: theme.spacing(2)
+  },
+  postTextWrapper: {
+    marginTop: theme.spacing(2)
+  }
+}))
+
 export default function Home() {
+  const classes = useHomeStyles()
   return (
-    <div className={styles.container}>
+    <Box
+    className={classes.root}
+    component="main">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>خانه - فوتوگرام</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Grid
+      container
+      className={classes.postsWrapper}
+      justify="center"
+      alignItems="flex-end"
+      wrap="wrap">
+        {
+          [].concat(feedPosts, feedPosts, feedPosts).map(post => {
+            return (
+              <Grid
+              item
+              xs={12}
+              md={4}
+              lg={3}>
+                <Card
+                className={classes.postCard}
+                elevation={0}>
+                  <CardHeader
+                  title={post.postedBy.name}
+                  subheader={post.location}
+                  avatar={<Avatar src={`/profiles/${post.postedBy.user_name}.jpg`} />} />
+                  <CardContent>
+                    <CardMedia
+                    className={classes.postPhoto}
+                    image={`/user_posts/${post.postedBy.user_name}/${post.media}`}/>
+                    <Grid
+                    container
+                    className={classes.postTextWrapper}
+                    direction="column"
+                    justify="space-evenly"
+                    alignItems="flex-start">
+                      <Grid
+                      item
+                      xs={12}>
+                        <Typography
+                        variant="body1">
+                          {post.likesCount} نفر پسندیده اند
+                        </Typography>
+                      </Grid>
+                      <Grid
+                      item
+                      className={classes.postActionsWrapper}>
+                        <IconButton
+                        color="secondary">
+                          <FavoriteBorderIcon />
+                        </IconButton>
+                        <IconButton>
+                          <CommentOutlinedIcon />
+                        </IconButton>
+                        <IconButton
+                        color="primary">
+                          <ShareIcon />
+                        </IconButton>
+                      </Grid>
+                      <Grid
+                      item>
+                        <Typography variant="body1">{post.postedBy.name}</Typography>
+                        <Typography variant="body2">{post.text}</Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
+    </Box>
   )
 }
